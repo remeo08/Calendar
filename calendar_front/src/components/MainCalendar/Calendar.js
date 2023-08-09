ae1c6c1df898f8346139219c175d4c1740adb74b
galim-kang <togarim@naver.com>
2023-08-09 18:40:53 +0900

    TUI카렌다 라이브러리 개시

--- calendar_front/src/components/MainCalendar/Calendar.js
+++ calendar_front/src/components/MainCalendar/Calendar.js
@@ -1,18 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from '@toast-ui/react-calendar';
 import '@toast-ui/calendar/dist/toastui-calendar.min.css';
 
 function TUICalendar() {
   const calendars = [{ id: 'cal1', name: 'Personal' }];
   const initialEvents = [
-    {
-      id: '1',
-      calendarId: 'cal1',
-      title: 'Lunch',
-      category: 'time',
-      start: '2022-06-28T12:00:00',
-      end: '2022-06-28T13:30:00',
-    },
     {
       id: '2',
       repeat: '',
@@ -25,28 +17,71 @@ function TUICalendar() {
       status: '상태',
       category: 'time',
       user: '유저',
-      team: '팀',
+      team: '어벤져스',
+    },
+    {
+      id: '3',
+      repeat: '',
+      start: '2023-08-12T15:00:00',
+      end: '2023-08-12T15:30:00',
+      calendarId: 'cal1',
+      title: 'event2',
+      description: '내용',
+      color: '색상',
+      status: '상태',
+      category: 'time',
+      user: '유저',
+      team: 'team1',
+    },
+    {
+      id: '4',
+      repeat: '',
+      start: '2023-08-20T15:00:00',
+      end: '2023-08-20T15:30:00',
+      calendarId: 'cal1',
+      title: 'event3',
+      description: '내용',
+      color: '색상',
+      status: '상태',
+      category: 'time',
+      user: '유저',
+      team: '가오갤',
     },
   ];
+
+  const filteredEvents = initialEvents.filter(
+    (event) => event.team === 'team1',
+  );
 
   const onAfterRenderEvent = (event) => {
     console.log(event.title);
   };
 
+  const [currentView, setCurrentView] = useState('month');
+
+  const changeView = (view) => {
+    setCurrentView(view);
+  };
+
   return (
     <div>
+      <button onClick={() => changeView('month')}>month</button>
+      <button onClick={() => changeView('week')}>week</button>
+      <button onClick={() => changeView('day')}>day</button>
       <Calendar
         height="420px"
-        view="month"
+        view={currentView}
         month={{
           dayNames: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-          visibleWeeksCount: 4,
+          // visibleWeeksCount: 4,
+          isAlways6Weeks: false,
         }}
         calendars={calendars}
-        events={initialEvents}
+        events={filteredEvents}
         onAfterRenderEvent={onAfterRenderEvent}
       />
     </div>
   );
 }
+
 export default TUICalendar;
