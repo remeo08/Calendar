5e7e0af04bff0ac50e9c7d3f1649f35d82a24a9d
byungyong <quddyddlrk@naver.com>
2023-08-10 13:19:47 +0900

    모델수정

--- calendar_back/nicknames/models.py
+++ calendar_back/nicknames/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Nickname(models.Model):
-    nickname = models.CharField(max_length=15, null=False)
-    description = models.CharField(max_length=150, null=True) # 소개
-    
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, default="")
+    nickname = models.CharField(max_length=15)
+    description = models.CharField(max_length=150, null=True)  # 소개
+
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.user
