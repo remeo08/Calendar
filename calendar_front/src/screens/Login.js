bc2930d171afc7139d4fa46bf326665646daabaf
ryoonago <133100923+ryoonago@users.noreply.github.com>
2023-08-11 17:06:01 +0900

    api 추가

--- calendar_front/src/screens/Login.js
+++ calendar_front/src/screens/Login.js
@@ -1,20 +1,46 @@
 import { useForm } from 'react-hook-form';
 import { styled } from 'styled-components';
 import Signup from './Signup';
+import axios from 'axios';
+
+const Container = styled.div`
+  // width: 400px;
+  // height: 500px;
+  // border: 1px solid black;
+  // display: flex;
+  // align-item: center;
+`;
+
 const LoginContainer = styled.div`
   width: 400px;
-  height: 800px;
+  height: 500px;
   border: 1px solid black;
   display: flex;
   align-item: center;
 `;
 
-const Login = ({
-  onSubmit = async (data) => {
-    await new Promise((r) => setTimeout(r, 1000));
-    alert(JSON.stringify(data));
-  },
-}) => {
+const Input = styled.input`
+  &:focus {
+    border-color: blue;
+    outline: none;
+  }
+`;
+
+const Form = styled.div``;
+
+const Login = () => {
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        'https://port-0-calendar-backend-ac2nll4pdsc1.sel3.cloudtype.app/api/v1/users/login/',
+        data,
+      ); // 서버로 로그인 요청 보내기
+      console.log(response.data); // 서버 응답 데이터 출력
+    } catch (error) {
+      console.error('로그인 실패:', error);
+    }
+  };
+
   const {
     register,
     handleSubmit,
@@ -22,12 +48,12 @@ const Login = ({
   } = useForm();
 
   return (
-    <>
+    <Container>
       <LoginContainer>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="id">아이디</label>
-          <input
-            id="id"
+          <Input
+            className="id"
             type="text"
             aria-invalid={
               isSubmitted ? (errors.id ? 'true' : 'false') : undefined
@@ -42,8 +68,8 @@ const Login = ({
           />
           {errors.id && <small role="alert">{errors.id.message}</small>}
           <label htmlFor="password">비밀번호</label>
-          <input
-            id="password"
+          <Input
+            className="password"
             type="password"
             aria-invalid={
               isSubmitted ? (errors.password ? 'true' : 'false') : undefined
@@ -62,10 +88,10 @@ const Login = ({
           <button type="submit" disabled={isSubmitting}>
             로그인
           </button>
-        </form>
+        </Form>
       </LoginContainer>
       <Signup></Signup>
-    </>
+    </Container>
   );
 };
 
