ea14bc87af7cb2212e4d98e4abcb214cb585ebc9
ryoonago <133100923+ryoonago@users.noreply.github.com>
2023-08-16 11:24:50 +0900

    아웃된 패키지 삭제 및 로그인 스타일 파일 추가

--- calendar_front/src/screens/Login.js
+++ calendar_front/src/screens/Login.js
@@ -4,29 +4,83 @@ import Signup from './Signup';
 import axios from 'axios';
 
 const Container = styled.div`
-  // width: 400px;
-  // height: 500px;
-  // border: 1px solid black;
-  // display: flex;
-  // align-item: center;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #ebecf0;
+  overflow: hidden;
 `;
 
 const LoginContainer = styled.div`
-  width: 400px;
-  height: 500px;
-  border: 1px solid black;
+  border-radius: 10px;
+  box-shadow:
+    -5px -5px 10px #fff,
+    5px 5px 10px #babebc;
+  position: absolute;
+  width: 768px;
+  min-height: 480px;
+  overflow: hidden;
+`;
+
+const Form = styled.div`
+  background: #ebecf0;
   display: flex;
-  align-item: center;
+  flex-direction: column;
+  padding: 0 50px;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Input = styled.input`
-  &:focus {
-    border-color: blue;
-    outline: none;
+background: #eee;
+padding: 16px;
+margin: 8px 0;
+width: 85%;
+border: 0;
+outline: none;
+border-radius: 20px;
+box-shadow: inset 7px 2px 10px #babebc, inset -5px -5px 12px #fff;
   }
 `;
 
-const Form = styled.div``;
+const Button = styled.button`
+  border-radius: 20px;
+  border: none;
+  outline: none;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 15px 45px;
+  margin: 14px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: transform 80ms ease-in;
+`;
+
+const SocialLinks = styled.div`
+  margin: 20px 0;
+`;
+
+const SocialLink = styled.div`
+  width: 40px;
+  height: 40px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+  border-radius: 50%;
+  box-shadow:
+    -5px -5px 10px #fff,
+    5px 5px 8px #babebc;
+  cursor: pointer;
+`;
+
+const SocialIcon = styled.i`
+  color: #000;
+`;
 
 const Login = () => {
   const onSubmit = async (data) => {
@@ -51,10 +105,33 @@ const Login = () => {
     <Container>
       <LoginContainer>
         <Form onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="id">아이디</label>
+          {/* <h1>Sign In</h1> */}
+          <SocialLinks>
+            <SocialLink>
+              <SocialIcon
+                className="fa fa-facebook"
+                aria-hidden="true"
+              ></SocialIcon>
+            </SocialLink>
+            <SocialLink>
+              <SocialIcon
+                className="fa fa-twitter"
+                aria-hidden="true"
+              ></SocialIcon>
+            </SocialLink>
+            <SocialLink>
+              <SocialIcon
+                className="fa fa-linkedin"
+                aria-hidden="true"
+              ></SocialIcon>
+            </SocialLink>
+          </SocialLinks>
+          {/* <span>or use your account</span> */}
+          {/* <label htmlFor="id">아이디</label> */}
           <Input
             className="id"
             type="text"
+            placeholder="아이디"
             aria-invalid={
               isSubmitted ? (errors.id ? 'true' : 'false') : undefined
             }
@@ -67,10 +144,11 @@ const Login = () => {
             })}
           />
           {errors.id && <small role="alert">{errors.id.message}</small>}
-          <label htmlFor="password">비밀번호</label>
+          {/* <label htmlFor="password">비밀번호</label> */}
           <Input
             className="password"
             type="password"
+            placeholder="비밀번호"
             aria-invalid={
               isSubmitted ? (errors.password ? 'true' : 'false') : undefined
             }
