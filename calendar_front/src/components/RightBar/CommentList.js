52b6790b2496fb2926575da80ad3fae9c2334e61
remeo08 <rmeorux5870@gmail.com>
2023-08-21 17:35:14 +0900

    댓글 수정, 삭제 기능 추가

--- calendar_front/src/components/RightBar/CommentList.js
+++ calendar_front/src/components/RightBar/CommentList.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import { styled } from 'styled-components';
 import CommentEdit from './CommentEdit';
 
+import {
+  //...
+  MdOutlineEdit, // 수정 아이콘
+  MdOutlineDelete, // 삭제 아이콘
+} from 'react-icons/md';
+
 const CommentItem = styled.div`
   padding: 7px;
 `;
@@ -31,12 +37,41 @@ const CommentContent = styled.div`
   word-wrap: break-word;
 `;
 
-const CommentList = ({ comment }) => {
+const CommentBtnGroup = styled.div`
+  display: flex;
+  // 원하는 스타일 적용
+`;
+
+const CommentList = ({ comment, removeComment, editComment }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [editedComment, setEditedComment] = useState(comment.description);
+
+  const handleEdit = () => {
+    if (editMode) {
+      editComment(comment.id, editedComment);
+    }
+    setEditMode(!editMode);
+  };
+
+  const handleDelete = () => {
+    if (window.confirm('댓글을 삭제하시겠습니까?')) {
+      removeComment(comment.id);
+    }
+  };
+
   return (
     <CommentItem>
       <CommentAuthor>{comment.author}</CommentAuthor>
       <CommentBox>
-        <CommentContent>{comment.description}</CommentContent>
+        {editMode ? (
+          <input
+            type="text"
+            value={editedComment}
+            onChange={(e) => setEditedComment(e.target.value)}
+          />
+        ) : (
+          <CommentContent>{comment.description}</CommentContent>
+        )}
         <CommentDate>
           {new Date(comment.createdTime).toISOString().slice(0, 10) +
             ' ' +
@@ -47,6 +82,14 @@ const CommentList = ({ comment }) => {
             })}
         </CommentDate>
       </CommentBox>
+      <CommentBtnGroup>
+        <button onClick={handleEdit}>
+          <MdOutlineEdit />
+        </button>
+        <button onClick={handleDelete}>
+          <MdOutlineDelete />
+        </button>
+      </CommentBtnGroup>
     </CommentItem>
   );
 };
